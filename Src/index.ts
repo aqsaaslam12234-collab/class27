36fff2d275e826226628180e12a81f7e7746acca
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-12-06 07:25:20 +0000

    save

--- Src/index.ts
+++ Src/index.ts
@@ -1,10 +1,21 @@
-const Str = "Civic";
-const reversedStr = Str.split().reverse().join();
-console.log(reversedStr);
-console.log ();
+// const Str = "Civic";
+// const reversedStr = Str.split().reverse().join();
+// console.log(reversedStr);
+// console.log ();
 
-for (let i=0; i <Str.replaceAll("", "").length; i++){
-    const clean = Str.replaceAll(" ", "");
-    const reversedString = clean[clean.length-1 -i];
+// for (let i=0; i <Str.replaceAll("", "").length; i++){
+//     const clean = Str.replaceAll(" ", "");
+//     const reversedString = clean[clean.length-1 -i];
+//     console.log(reversedString);
+// }
+
+const Str = "Civic";
+
+const reversedStr = Str.split("").reverse().join("");
+console.log(reversedStr);
+
+const clean = Str.replaceAll(" ", "");
+for (let i = 0; i < clean.length; i++) {
+    const reversedString = clean[clean.length - 1 - i];
     console.log(reversedString);
 }
